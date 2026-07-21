1ac6f2d943c4d5f543ab92160c17b77819f2fb07
khaledAlnouby <121772428+khaledAlnouby@users.noreply.github.com>
2026-07-21 21:26:18 +0300

    Stage 1: root and health endpoints

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
+app.get("/", (req,res) => {
+    res.send({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
+});
 
+app.get("/health", (req,res) => {
+    res.send({ "status": "ok" });
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
-})
+});
